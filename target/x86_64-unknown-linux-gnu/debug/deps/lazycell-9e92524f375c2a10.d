a5d0f14a0f7e6f8372afce7454e87214711fa43d
Cooki-e <1291852105@qq.com>
2023-07-05 11:17:23 +0800

    feat: add sourcecode of rust-lazycell
    It provides a struct similar to Cell that can be lazily filled.
    
    Log: add sourcecode of rust-lazycell
    issue: deepin-commiunity/sig-deepin-sysdev-teams#77

--- target/x86_64-unknown-linux-gnu/debug/deps/lazycell-9e92524f375c2a10.d
+++
@@ -1,5 +0,0 @@
-/home/Cookie/SRC/rust-cell/rust-lazycell-1.3.0/target/x86_64-unknown-linux-gnu/debug/deps/lazycell-9e92524f375c2a10: src/lib.rs
-
-/home/Cookie/SRC/rust-cell/rust-lazycell-1.3.0/target/x86_64-unknown-linux-gnu/debug/deps/lazycell-9e92524f375c2a10.d: src/lib.rs
-
-src/lib.rs:
